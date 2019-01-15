828b52e3834e8e49884386310c4dba3f1dc7b42a
godxvincent <godxvincent@gmail.com>
2019-01-14 22:43:13 -0500

    The file manage was added and the views and templates were changed to show how CBV works

--- webplayground/core/views.py
+++ webplayground/core/views.py
@@ -1,7 +1,26 @@
+# Link de plantillas de vistas basadas en clases (CBV)
+# https://ccbv.co.uk/
+# Link vistas basadas en clases documentación oficialself.
+# https://docs.djangoproject.com/en/2.0/ref/class-based-views/
+# Link de clases de tipo TEMPLATES
+# https://docs.djangoproject.com/en/2.0/ref/class-based-views/base/#templateview
+
+from django.views.generic.base import TemplateView
 from django.shortcuts import render
 
-def home(request):
-    return render(request, "core/home.html")
 
-def sample(request):
-    return render(request, "core/sample.html")
+class HomePageView(TemplateView):
+    template_name = "core/home.html"
+
+    # Esta es una forma de pasar un diccionario de contexto.
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['titulo'] = 'Mi titulo a través de un diccionario'
+        return context
+
+
+class SamplePageView(TemplateView):
+    template_name = "core/sample.html"
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, {'titulo': 'Titulo de la página sample'})
