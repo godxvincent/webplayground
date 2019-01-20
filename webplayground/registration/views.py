bffe2e66f0c09b3188fd4de0aa1ee0b3cb584c70
godxvincent <godxvincent@gmail.com>
2019-01-20 13:01:06 -0500

    The registration form was added and the template was changed to have a better look

--- webplayground/registration/views.py
+++ webplayground/registration/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
+from django import forms
 
 # Create your views here.
 
@@ -11,3 +12,16 @@ class SignUpView(CreateView):
 
     def get_success_url(self):
         return reverse_lazy('login') + '?registration'
+
+    def get_form(self, form=None):
+        new_form = super(SignUpView, self).get_form()
+        # Los nombres de los campos hay que sacarlos de los nombres que genera automaticamente
+        # Django, por tanto toca entrar en el codigo generado visto a través del navegador
+
+        new_form.fields['username'].widget = forms.TextInput(
+            attrs={'class': 'form-control mb-2', 'placeholder': 'Nombre de usuario'})
+        new_form.fields['password1'].widget = forms.PasswordInput(
+            attrs={'class': 'form-control mb-2', 'placeholder': 'Contraseña'})
+        new_form.fields['password2'].widget = forms.PasswordInput(
+            attrs={'class': 'form-control mb-2', 'placeholder': 'Repite contraseña'})
+        return new_form
