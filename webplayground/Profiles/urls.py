9ebcfe34856f08a4f4c53ef3fa8ab1e6210636da
godxvincent <godxvincent@gmail.com>
2019-02-03 15:41:44 -0500

    The url problem was fixed

--- webplayground/Profiles/urls.py
+++ webplayground/Profiles/urls.py
@@ -3,5 +3,5 @@ from .views import ProfileListView, ProfileDetailView
 
 urlpatterns = [
     path('', ProfileListView.as_view(), name='profiles'),
-    path('<slug:username>/', ProfileDetailView.as_view(), name='profile_detail'),
+    path('<username>/', ProfileDetailView.as_view(), name='profile_detail'),
 ]
